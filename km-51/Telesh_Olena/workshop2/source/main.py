cdc5930da111dd9f87dd8525f45f758be72bb31e
HelenTelesh <mila_dracon@ukr.net>
2018-11-12 14:13:45 +0200

    Add files via upload

--- km-51/Telesh_Olena/workshop2/source/main.py
+++ km-51/Telesh_Olena/workshop2/source/main.py
@@ -1,8 +1,37 @@
-"""
-Тут написати умову до завдання
-"""
+from flask import Flask
 
+app = Flask(__name__)
 
+user_dict = {
+    "login" : "user1",
+    "passw" : "1111",
+    "user_role" : "student",
+    "user_name" : "Ivan",
+    "surname" : "Sydorov",
+    "fathername" : "Ivanovich"
+    }
 
-if __name__ == "__main__":
-    #TODO
+record_book_dict = {
+    "university_name" : "KPI",
+    "faculty_name" : "Applied Mathemetics",
+    "group_number" : "1",
+    "login" : "user1",
+    "book_num" : "AB111"
+    }
+
+@app.route('/server/api/<action>', methods = ['POST', 'GET'])
+def processing(action):
+    if (action == 'record_book'):
+        return render_template('record_book_temp.html')
+    elif(action == 'user'):
+        return render_template('user_temp.html')
+    elif (action == 'all'):
+        if request.method == 'POST':
+            result = request.form
+            return render_template("all.html", result = result)
+    else:
+        # Eror 404.
+        Flask.abort(404)
+
+app.run(debug=True, port= 8085)
+
